dfcd5d8520f2012bb3cb9e4ec3c380e1c5cc0777
Zhou Shuai <shuai.zhou@ipanda.co.jp>
2022-04-13 09:21:30 +0800

    Add exception common class

---
+++ src/main/java/cn/dlysxx/www/common/exception/SystemException.java
@@ -0,0 +1,60 @@
+package cn.dlysxx.www.common.exception;
+
+/**
+ * BusinessException class.
+ *
+ * @author shuai.zhou
+ */
+public class SystemException extends RuntimeException {
+
+    private static final long serialVersionUID = 1321372647800456847L;
+
+    /** error code */
+    protected String code;
+
+    /** error message */
+    protected String message;
+
+    /**
+     * Constructor.
+     *
+     */
+    public SystemException(String code) {
+        this(code, null);
+    }
+
+    /**
+     * Constructor.
+     *
+     */
+    public SystemException(String code, String message) {
+        this(code, message, null);
+    }
+
+    /**
+     * Constructor.
+     *
+     */
+    public SystemException(String code, String message, Throwable cause) {
+        super(cause);
+        this.code = code;
+        this.message = message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
